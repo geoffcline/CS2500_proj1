832e5d839ac4f2dc4fd155407511fd8b5a7cc031
Geoffrey Cline <geoffreyc@outlook.com>
2014-10-28 19:08:08 -0500

    working on linker

--- Greedy.h
+++ Greedy.h
@@ -6,14 +6,6 @@
 
 using namespace std;
 
-void QuickSortP(KS_List& work);
 
-template <typename T>
-void QuickSortP(T** work, const int start, const int end);
-
-template <typename T>
-int PivotP(T** work, const int start, const int end);
-
-int GreedyKS(const KS_List A, const int maxw);
 
 #endif 

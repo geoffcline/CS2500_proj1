a6ce7d6a67ccb7201895b2f6d247df8a61aafdb9
Geoffrey Cline <geoffreyc@outlook.com>
2014-10-28 19:23:41 -0500

    split back up part 2

---
+++ test.cpp
@@ -0,0 +1,36 @@
+#include "Greedy.h"
+#include "KnapSack.h"
+#include <fstream>
+
+using namespace std;
+
+int main ()
+{
+  clock_t t;
+  
+  ofstream fout;
+  fout.open("table.csv");
+  
+  int maxw;
+  int resultv;
+    
+  const float SIGMA = 0;
+  const float MU = 0;
+  const int   SIZE = 0;
+  const int   TESTS = 0;
+  
+
+  KS_List L_working(SIGMA, MU, SIZE);
+
+  for (int i = 0; i < TESTS; ++i)
+  {
+    L_working.generate();
+    t = clock();
+              //WHY DON'T YOU KNOW GREEDYKS? 
+    resultv = GreedyKS(L_working, maxw);
+    t = clock() - t;
+    fout << "\"GREEDY\",\"" << i << "\",\"" << t << "\"" << endl;
+  }
+
+  return 0;
+}

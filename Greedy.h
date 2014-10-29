a19789af20a756165341df37d33d0ad9bf9d1306
Geoffrey Cline <geoffreyc@outlook.com>
2014-10-28 19:23:29 -0500

    split back up into files
    resolved linker error so far

--- Greedy.h
+++ Greedy.h
@@ -1,5 +1,19 @@
 #ifndef GREEDY_H
 #define GREEDY_H
 
+#include "KnapSack.h"
+
+int GreedyKS(const KS_List& A, const int maxw);
+
+void QuickSortP(KS_List& work);
+
+template <typename T>
+void QuickSortP(T** work, const int start, const int end);
+
+template <typename T>
+int PivotP(T** work, const int start, const int end);
+
+template <typename T>
+void swap(T lhs, T rhs);
 
 #endif 
